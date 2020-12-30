8bf94c8e72dfcc9f3379abcfc908e2837a408e4a
tamercitynews <abdelmaaboud.tamer@gmail.com>
2020-12-30 14:59:42 +0100

    Added models

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.onload = () => {
   const button = document.querySelector('button[data-action="change"]');
-  button.innerText = 'Change';
+  button.innerText = '﹖';
 
   let places = staticLoadPlaces();
   renderPlaces(places);
@@ -27,23 +27,35 @@ function staticLoadPlaces() {
 
 var models = [
   {
+    url: './assets/magnemite/scene.gltf',
+    scale: '0.5 0.5 0.5',
+    info: 'Magnemite, Lv. 5, HP 10/10',
+    rotation: '0 180 0',
+  },
+  {
+      url: './assets/articuno/scene.gltf',
+      scale: '0.2 0.2 0.2',
+      rotation: '0 180 0',
+      info: 'Articuno, Lv. 80, HP 100/100',
+  },
+  {
+      url: './assets/dragonite/scene.gltf',
+      scale: '0.08 0.08 0.08',
+      rotation: '0 180 0',
+      info: 'Dragonite, Lv. 99, HP 150/150',
+  },
+  {
       url: './assets/kratos/scene.gltf',
       scale: '2 2 2',
       rotation: '180 0 0',
       info: 'Articuno, Lv. 80, HP 100/100',
   },
   {
-      url: './assets/magnemite/scene.gltf',
-      scale: '2 2 2',
-      info: 'Magnemite, Lv. 5, HP 10/10',
-      rotation: '180 0 0',
-  },
-  {
       url: './assets/scene/scene.gltf',
       scale: '2 2 2',
       rotation: '180 0 0',
       info: 'Dragonite, Lv. 99, HP 150/150',
-  }
+  },
 ];
 
 var modelIndex = 0;
