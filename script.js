6c870d648d2ea3398ef491f507f9c039253ccfeb
tamercitynews <abdelmaaboud.tamer@gmail.com>
2020-12-30 14:08:53 +0100

    Changed scale value

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ function renderPlaces(places) {
      model.setAttribute('gltf-model', './assets/magnemite/scene.gltf');
      model.setAttribute('rotation', '0 0 0');
      model.setAttribute('animation-mixer', '');
-     model.setAttribute('scale', '0.5 0.5 0.5');
+     model.setAttribute('scale', '1.5 1.5 1.5');
 
      model.addEventListener('loaded', () => {
          window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
